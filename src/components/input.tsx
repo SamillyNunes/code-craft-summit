dce62edc5fe6f929326fc81c2009d38f95a5d816
Samilly <samillynunes19@gmail.com>
2025-02-18 11:30:28 -0300

    feat: change input icon color when has a value

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -7,7 +7,10 @@ export function Input(props: InputProps) {
     return (
         <div className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100">
             {/* esse group vai definir que, quando o meu grupo (no caso a div) estiver com foco, faca isso... */}
-            <span className="text-gray-400 group-focus-within:text-gray-100">
+            {/* o comando "group-[&:not(:has(input:placeholder-shown))]" vai dizer que, se o input nao tiver mostrando o placeholder, no grupo, vai fazer o que diz depois dos dois pontos */}
+            <span
+                className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100"
+            >
                 <MailIcon />
             </span>
             <input
