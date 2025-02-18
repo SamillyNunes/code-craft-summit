d881431d8b1d9d34d96add2103fc39745b08241b
Samilly <samillynunes19@gmail.com>
2025-02-18 09:37:54 -0300

    chore: buttons adjusted to extends default button props

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,14 @@
-import { ReactNode } from "react";
+import { ComponentProps, ReactNode } from "react";
 
-interface ButtonProps {
-    children: ReactNode
-}
+// Aqui estamos extendendo todas as props padrao de um button para nao ter que especificar manualmente
+// Nesse caso tambem nao precisamos especificar children, ja que esta eh uma propriedade que tambem vem nas props de button
+interface ButtonProps extends ComponentProps<'button'>{};
 
 export default function Button(props: ButtonProps) {
     return (
-        <button className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900">
-            {props.children}
-        </button>
+        <button 
+            className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+            {...props}
+        />
     );
 }
