03cc297fc132f7ceeb1c018b398a13a8a6fa3c09
Samilly <samillynunes19@gmail.com>
2025-02-18 11:35:18 -0300

    feat: present error colors when input has an error

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,15 +1,20 @@
 import { MailIcon } from "lucide-react";
 import { ComponentProps } from "react";
 
-interface InputProps extends ComponentProps<'input'> { }
+interface InputProps extends ComponentProps<'input'> {
+    error?: boolean
+}
 
-export function Input(props: InputProps) {
+export function Input({error=false, ...props}: InputProps) {
     return (
-        <div className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100">
+        <div 
+            data-error={error}
+            className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100 data-[error=true]:border-danger"
+        >
             {/* esse group vai definir que, quando o meu grupo (no caso a div) estiver com foco, faca isso... */}
             {/* o comando "group-[&:not(:has(input:placeholder-shown))]" vai dizer que, se o input nao tiver mostrando o placeholder, no grupo, vai fazer o que diz depois dos dois pontos */}
             <span
-                className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100"
+                className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
             >
                 <MailIcon />
             </span>
