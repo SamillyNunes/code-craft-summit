18d210878e779f9ee03168aa7ad8825b3c9ccf46
Samilly <samillynunes19@gmail.com>
2025-02-18 13:45:15 -0300

    feat: about event card created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Radio } from 'lucide-react';
 import Image from 'next/image';
 import logo from '../assets/logo.svg';
 
@@ -12,6 +13,35 @@ export default function Home() {
 						<span className="text-blue">CodeCraft</span>
 						Summit 2025
 					</h1>
+				</div>
+
+				<div className="flex gap-5 items-stretch flex-col md:flex-row">
+					<div className="flex-1 bg-gray-700 border-gray-600 rounded-2xl p-8 space-y-6">
+						<div className="flex items-center justify-between">
+							<h2 className="font-heading font-semibold text-gray-200 text-xl">
+								Sobre o evento
+							</h2>
+							<span className="text-purple font-semibold text-xs flex items-center gap-2">
+								<Radio className="size-5" />
+								AO VIVO
+							</span>
+						</div>
+
+						<p className="text-gray-300 leading-relaxed text-sm md:text-base">
+							Um evento feito por e para pessoas desenvolvedoras apaixonadas por
+							criar soluções inovadoras e compartilhar conhecimento. Vamos
+							mergulhar nas tendências mais recentes em desenvolvimento de
+							software, arquitetura de sistemas e tecnologias emergentes, com
+							palestras, workshops e hackathons.
+							<br />
+							<br />
+							Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
+						</p>
+					</div>
+
+					<form action="" className="flex-1">
+						form
+					</form>
 				</div>
 			</div>
 		</main>
