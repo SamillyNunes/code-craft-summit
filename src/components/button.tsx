fd4e71da095aff1a6cc71a33b90839bcdcafe0ef
Samilly <samillynunes19@gmail.com>
2025-02-18 09:39:32 -0300

    chore: removed export default from buttons

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,7 +4,7 @@ import { ComponentProps, ReactNode } from "react";
 // Nesse caso tambem nao precisamos especificar children, ja que esta eh uma propriedade que tambem vem nas props de button
 interface ButtonProps extends ComponentProps<'button'>{};
 
-export default function Button(props: ButtonProps) {
+export function Button(props: ButtonProps) {
     return (
         <button 
             className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
--- src/components/icon_button.tsx
+++ src/components/icon_button.tsx
@@ -2,7 +2,7 @@ import { ComponentProps, ReactNode } from "react";
 
 interface IconButtonProps extends ComponentProps<'button'>{};
 
-export default function IconButton(props: IconButtonProps) {
+export function IconButton(props: IconButtonProps) {
     return (
         <button 
             className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
