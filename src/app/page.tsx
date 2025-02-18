bc547dd2e099309a9b2ffe756a09a5d9a6fa8bcf
Samilly <samillynunes19@gmail.com>
2025-02-18 13:52:39 -0300

    feat: register form created and home finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-import { Radio } from 'lucide-react';
+import { Button } from '@/components/button';
+import { InputField, InputIcon, InputRoot } from '@/components/input';
+import { ArrowRight, Mail, Radio, User } from 'lucide-react';
 import Image from 'next/image';
 import logo from '../assets/logo.svg';
 
@@ -39,8 +41,33 @@ export default function Home() {
 						</p>
 					</div>
 
-					<form action="" className="flex-1">
-						form
+					<form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
+						<h2 className="font-heading font-semibold text-gray-200 text-xl">
+							Inscrição
+						</h2>
+
+						<div className="space-y-3">
+							<InputRoot>
+								<InputIcon>
+									<User />
+								</InputIcon>
+
+								<InputField type="text" placeholder="Nome completo" />
+							</InputRoot>
+
+							<InputRoot>
+								<InputIcon>
+									<Mail />
+								</InputIcon>
+
+								<InputField type="email" placeholder="E-mail" />
+							</InputRoot>
+						</div>
+
+						<Button type="submit">
+							Confirmar
+							<ArrowRight />
+						</Button>
 					</form>
 				</div>
 			</div>
